0147892ec43a2efebef45cec7a2cdc25b611c2aa
Gabriel Gontariu <gabi@openline.ai>
2022-10-25 13:39:43 +0300

    local deployment

---
+++ deployment/k8s/local-minikube/0-build-deploy-openline-oasis-local-images.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# Deploy Images
+NAMESPACE_NAME="openline-development"
+
+## Build Images
+cd  $OASIS_HOME
+docker build -t message-store -f message-store/Dockerfile .
+docker build -t oasis-api -f oasis-api/Dockerfile .
+docker build -t channels-api -f channels-api/Dockerfile .
+
+minikube image load message-store:latest
+minikube image load oasis-api:latest
+minikube image load channels-api:latest
+
+cd $OASIS_HOME/deployment/k8s/local-minikube
+kubectl apply -f apps-config/message-store.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/message-store-k8s-service.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/oasis-api.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/oasis-api-k8s-service.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/channels-api.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/channels-api-k8s-service.yaml --namespace $NAMESPACE_NAME
