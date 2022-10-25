123471718697c7fb2c9bf6ae07661d8af9a1f572
tsearle <torrey@openline.ai>
2022-10-25 11:58:33 +0200

    make build script match

--- oasis-voice/kamailio/build-docker.sh
+++ oasis-voice/kamailio/build-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t ghcr.io/openline-kamailio-server --build-arg ARCH=amd64/ .
+docker build -t ghcr.io/openline-ai/openline-oasis/openline-kamailio-server --build-arg ARCH=amd64/ .
