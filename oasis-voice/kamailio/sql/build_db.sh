41cb2fb546ba53136a2bdb2d8db81a1334dd8731
tsearle <torrey@openline.ai>
2022-10-26 16:28:06 +0200

    get a database setup

--- oasis-voice/kamailio/sql/build_db.sh
+++ oasis-voice/kamailio/sql/build_db.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
-cat standard-create.sql permissions-create.sql carriers.sql | PGPASSWORD=$SQL_PASSWORD  psql -h $SQL_HOST $SQL_USER $SQL_DATABASE
+FILES="standard-create.sql permissions-create.sql carriers.sql"
+if [ "x$1" == "xlocal-kube" ]; then
+  pod=$(kubectl get pods -n openline-development|grep oasis-postgres| cut -f1 -d ' ')
+  echo $FILES |xargs cat|kubectl exec -it $pod -- psql $SQL_USER $SQL_DATABASE
+else
+  echo $FILES |xargs cat| PGPASSWORD=$SQL_PASSWORD  psql -h $SQL_HOST $SQL_USER $SQL_DATABASE
+fi
