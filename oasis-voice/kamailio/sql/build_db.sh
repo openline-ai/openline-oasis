92147b5d0a31a6297d14385b5c7b6874c60a1ec2
tsearle <torrey@openline.ai>
2022-10-27 14:55:53 +0200

    wait for db startup (#56)

--- oasis-voice/kamailio/sql/build_db.sh
+++ oasis-voice/kamailio/sql/build_db.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 FILES="standard-create.sql permissions-create.sql carriers.sql"
 if [ "x$1" == "xlocal-kube" ]; then
-  pod=$(kubectl get pods -n openline-development|grep oasis-postgres| cut -f1 -d ' ')
+  while [ -z "$pod" ]; do
+    pod=$(kubectl get pods -n openline-development|grep oasis-postgres|grep Running| cut -f1 -d ' ')
+    if [ -z "$pod" ]; then
+      echo "database not ready waiting"
+      sleep 1
+    fi
+  done
   echo $FILES |xargs cat|kubectl exec -n openline-development -it $pod -- psql -U $SQL_USER $SQL_DATABASE
 else
   echo $FILES |xargs cat| PGPASSWORD=$SQL_PASSWORD  psql -h $SQL_HOST $SQL_USER $SQL_DATABASE
