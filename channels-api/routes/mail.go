f450270c479f6c39359fdea7a0e216db7c41a091
tsearle <torrey@openline.ai>
2022-11-03 11:26:26 +0100

    make mail api more robust

--- channels-api/routes/mail.go
+++ channels-api/routes/mail.go
@@ -32,16 +32,23 @@ func addMailRoutes(rg *gin.RouterGroup) {
 	mail.POST("/", func(c *gin.Context) {
 		var req MailPostRequest
 		if err := c.BindJSON(&req); err != nil {
-			log.Fatalf("unable to parse json: %v", err)
-			// DO SOMETHING WITH THE ERROR
+			log.Printf("unable to parse json: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"result": fmt.Sprintf("unable to parse json: %v", err),
+			})
+			return
 		}
-		c.JSON(http.StatusOK, "Mail POST endpoint. req sent: sender "+req.Sender+"; raw message: "+req.RawMessage)
+		//c.JSON(http.StatusOK, "Mail POST endpoint. req sent: sender "+req.Sender+"; raw message: "+req.RawMessage)
 
 		log.Printf("Got message from %s", req.Sender)
 		mailReader := strings.NewReader(req.RawMessage)
 		email, err := parsemail.Parse(mailReader) // returns Email struct and error
 		if err != nil {
-			log.Fatalf("Unable to parse Email: %v", err)
+			log.Printf("Unable to parse Email: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"result": fmt.Sprintf("Unable to parse Email: %v", err),
+			})
+			return
 		}
 		// Contact the server and print out its response.
 		mi := &pb.Message{
@@ -54,7 +61,11 @@ func addMailRoutes(rg *gin.RouterGroup) {
 		//Set up a connection to the server.
 		conn, err := grpc.Dial(conf.Service.MessageStore, grpc.WithInsecure())
 		if err != nil {
-			log.Fatalf("did not connect: %v", err)
+			log.Printf("did not connect: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"result": fmt.Sprintf("did not connect: %v", err),
+			})
+			return
 		}
 		defer conn.Close()
 		client := pb.NewMessageStoreServiceClient(conn)
@@ -65,12 +76,16 @@ func addMailRoutes(rg *gin.RouterGroup) {
 
 		if err != nil {
 			se, _ := status.FromError(err)
-			log.Fatalf("failed creating message item: status=%s message=%s", se.Code(), se.Message())
+			log.Printf("failed creating message item: status=%s message=%s", se.Code(), se.Message())
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"result": fmt.Sprintf("failed creating message item: status=%s message=%s", se.Code(), se.Message()),
+			})
+			return
 		}
 		log.Printf("message item created with id: %d", *message.Id)
 
 		c.JSON(http.StatusOK, gin.H{
-			"result": fmt.Sprint("created"),
+			"result": fmt.Sprintf("message item created with id: %d", *message.Id),
 		})
 	})
 }
