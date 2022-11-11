9f9a87c7216b6bea7e14a642ebaff188b631fa72
Gabriel Gontariu <gabi@openline.ai>
2022-11-11 13:52:34 +0200

    112 websocket fixes

--- oasis-api/routes/websockets.go
+++ oasis-api/routes/websockets.go
@@ -9,7 +9,12 @@ import (
 	"openline-ai/oasis-api/hub"
 )
 
-var upgrader = websocket.Upgrader{
+var msgUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var feedUpgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
@@ -21,42 +26,30 @@ type Message struct {
 func addWebSocketRoutes(rg *gin.RouterGroup, fh *hub.FeedHub, mh *hub.MessageHub) {
 
 	rg.GET("/ws", func(c *gin.Context) {
-		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+		feedUpgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		ws, err := msgUpgrader.Upgrade(c.Writer, c.Request, nil)
 		if !errors.Is(err, nil) {
 			log.Println(err)
 		}
 		defer ws.Close()
 		fh.Clients[ws] = true
-		read(ws)
 		log.Println("Connected!")
+		select {}
 	})
 
 	rg.GET("/ws/:feedId", func(c *gin.Context) {
 		feedId := c.Param("feedId")
-		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+		log.Println(feedId)
+		msgUpgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		ws, err := msgUpgrader.Upgrade(c.Writer, c.Request, nil)
 		if !errors.Is(err, nil) {
 			log.Println(err)
 		}
 		defer ws.Close()
 		mh.Clients[feedId] = ws
 		log.Println("Connected!")
-		read(ws)
+		select {}
 	})
-}
-
-func read(ws *websocket.Conn) {
-	for {
-		var message Message
-		err := ws.ReadJSON(&message)
-		if !errors.Is(err, nil) {
-			log.Printf("error occurred: %v", err)
-			//delete(hub.clients, ws)
-			break
-		}
-		log.Println(message)
-	}
 }
