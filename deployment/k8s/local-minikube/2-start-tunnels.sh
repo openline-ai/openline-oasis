9f48541d2784f3a47b34349300ae48bb6f31ef82
Gabriel Gontariu <gabi@openline.ai>
2022-11-11 10:16:49 +0200

    85 implement websocket support for oasis api

--- deployment/k8s/local-minikube/2-start-tunnels.sh
+++ deployment/k8s/local-minikube/2-start-tunnels.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 export NAMESPACE_NAME=oasis-dev
-kubectl port-forward --namespace $NAMESPACE_NAME svc/oasis-frontend-service 3006:3006 &
-kubectl port-forward --namespace $NAMESPACE_NAME svc/oasis-api-service 8006:8006 &
-kubectl port-forward --namespace $NAMESPACE_NAME svc/channels-api-service 8013:8013 &
-
+minikube tunnel &
 if [ $(uname -m) == "x86_64" ];
 then
   kubectl port-forward --namespace $NAMESPACE_NAME svc/kamailio-service 8080:8080 &
