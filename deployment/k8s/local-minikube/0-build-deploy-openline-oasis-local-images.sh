0b75de0ef02a1520c4dc7f1005552c215a9b3293
Gabriel Gontariu <gabi@openline.ai>
2022-11-11 10:20:09 +0200

    85 implement websocket support for oasis api

--- deployment/k8s/local-minikube/0-build-deploy-openline-oasis-local-images.sh
+++ deployment/k8s/local-minikube/0-build-deploy-openline-oasis-local-images.sh
@@ -168,8 +168,10 @@ kubectl apply -f apps-config/oasis-api.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/oasis-api-k8s-service.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/channels-api.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/channels-api-k8s-service.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/channels-api-k8s-loadbalancer.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/oasis-frontend.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/oasis-frontend-k8s-service.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/oasis-frontend-k8s-loadbalancer.yaml --namespace $NAMESPACE_NAME
 
 if [ $(uname -m) == "x86_64" ];
 then
