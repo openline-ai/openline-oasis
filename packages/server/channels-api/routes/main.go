33ab877eb1dbbd0f0db2ae605b6114aeb2eb6590
edifirut <108661145+edifirut@users.noreply.github.com>
2023-01-04 04:37:26 -0800

    Remove CORS from channels (#250)

--- packages/server/channels-api/routes/main.go
+++ packages/server/channels-api/routes/main.go
@@ -7,7 +7,6 @@ import (
 	c "openline-ai/channels-api/config"
 	"openline-ai/channels-api/routes/chatHub"
 	"openline-ai/channels-api/util"
-	"strings"
 )
 
 // Run will start the server
@@ -22,7 +21,7 @@ func getRouter(conf *c.Config, fh *chatHub.Hub) *gin.Engine {
 	router := gin.New()
 	corsConfig := cors.DefaultConfig()
 
-	corsConfig.AllowOrigins = strings.Split(conf.Service.CorsUrl, " ")
+	corsConfig.AllowOrigins = []string{"*"}
 	// To be able to send tokens to the server.
 	corsConfig.AllowCredentials = true
 
