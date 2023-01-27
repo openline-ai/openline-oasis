b167c7e726715755647138e36306b94d7de66a6e
Eduard Firut <edi@openline.ai>
2023-01-27 09:27:32 +0200

    Update readme Use username header for call credentials instead of param

--- packages/server/oasis-api/routes/callcredential.go
+++ packages/server/oasis-api/routes/callcredential.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	commonService "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/service"
 	c "github.com/openline-ai/openline-oasis/packages/server/oasis-api/config"
 	"github.com/openline-ai/openline-oasis/packages/server/oasis-api/util"
 	"net/http"
@@ -13,7 +14,7 @@ func addCallCredentialRoutes(rg *gin.RouterGroup, conf *c.Config) {
 
 	rg.GET("/call_credentials", func(c *gin.Context) {
 		expiresTime := time.Now().Unix() + int64(conf.WebRTC.TTL)
-		timeLimitedUser := fmt.Sprintf("%d:%s", expiresTime, c.Query("username"))
+		timeLimitedUser := fmt.Sprintf("%d:%s", expiresTime, c.GetHeader(commonService.UsernameHeader))
 		password := util.GetSignature(timeLimitedUser, conf.WebRTC.AuthSecret)
 		c.JSON(http.StatusOK, gin.H{"username": timeLimitedUser, "password": password, "ttl": conf.WebRTC.TTL})
 
