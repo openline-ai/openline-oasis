f08de1771f91d4767253dfda95b5b2c7671d8998
alexopenline <alex@openline.ai>
2023-01-27 12:18:45 +0200

    Adapt oasis-ui with latest changes in user to tenant lookup

--- packages/server/oasis-api/config/env_config.go
+++ packages/server/oasis-api/config/env_config.go
@@ -24,4 +24,12 @@ type Config struct {
 		TTL          int    `env:"WEBRTC_AUTH_TTL,required"`
 		PingInterval int    `env:"WEBSOCKET_PING_INTERVAL"`
 	}
+	Neo4j struct {
+		Target                string `env:"NEO4J_TARGET,required"`
+		User                  string `env:"NEO4J_AUTH_USER,required,unset"`
+		Pwd                   string `env:"NEO4J_AUTH_PWD,required,unset"`
+		Realm                 string `env:"NEO4J_AUTH_REALM"`
+		MaxConnectionPoolSize int    `env:"NEO4J_MAX_CONN_POOL_SIZE" envDefault:"100"`
+		LogLevel              string `env:"NEO4J_LOG_LEVEL" envDefault:"WARNING"`
+	}
 }
