12b87e22015875c24e06a57275f75ae2328f6669
tsearle <torrey@openline.ai>
2023-03-01 17:45:55 +0100

    implement vcon endpoint

--- packages/server/channels-api/routes/vcon.go
+++ packages/server/channels-api/routes/vcon.go
@@ -1,15 +1,61 @@
 package routes
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/service"
 	ms "github.com/openline-ai/openline-customer-os/packages/server/message-store-api/proto/generated"
 	c "github.com/openline-ai/openline-oasis/packages/server/channels-api/config"
 	"github.com/openline-ai/openline-oasis/packages/server/channels-api/model"
 	"github.com/openline-ai/openline-oasis/packages/server/channels-api/util"
+	o "github.com/openline-ai/openline-oasis/packages/server/oasis-api/proto/generated"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 	"net/http"
 )
+
+func encodePartyToParticipantId(party *model.VConParty) *ms.ParticipantId {
+	if party.Mailto != nil {
+		return &ms.ParticipantId{
+			Type:       ms.ParticipantIdType_MAILTO,
+			Identifier: *party.Mailto,
+		}
+	} else if party.Tel != nil {
+		return &ms.ParticipantId{
+			Type:       ms.ParticipantIdType_TEL,
+			Identifier: *party.Tel,
+		}
+	}
+	return nil
+}
+
+func getInitator(req *model.VCon) *ms.ParticipantId {
+	if len(req.Dialog) == 0 {
+		return nil
+	}
+	if len(req.Parties) == 0 {
+		return nil
+	}
+	if len(req.Dialog[0].Parties) == 0 {
+		return encodePartyToParticipantId(&req.Parties[0])
+	}
+	if req.Dialog[0].Parties[0] > int64(len(req.Parties)) {
+		return encodePartyToParticipantId(&req.Parties[0])
+	}
+	return encodePartyToParticipantId(&req.Parties[req.Dialog[0].Parties[0]])
+}
+
+func getUser(req *model.VCon) string {
+
+	for _, p := range req.Parties {
+		if p.Mailto != nil {
+			return *p.Mailto
+		}
+	}
+	return ""
+}
 
 func AddVconRoutes(conf *c.Config, df util.DialFactory, rg *gin.RouterGroup) {
 	rg.POST("/vcon", func(c *gin.Context) {
@@ -26,13 +72,55 @@ func AddVconRoutes(conf *c.Config, df util.DialFactory, rg *gin.RouterGroup) {
 			c.JSON(http.StatusForbidden, gin.H{"result": "Invalid API Key"})
 			return
 		}
+		threadId := req.UUID
+		if req.Appended != nil {
+			threadId = req.Appended.UUID
+		}
+		participants := make([]*ms.ParticipantId, len(req.Parties))
+		for i, p := range req.Parties {
+			participants[i] = encodePartyToParticipantId(&p)
+		}
 		message := &ms.InputMessage{
 			Type:                    ms.MessageType_VOICE,
 			Subtype:                 ms.MessageSubtype_MESSAGE,
 			Content:                 &req.Dialog[0].Body,
 			Direction:               ms.MessageDirection_INBOUND,
-			InitiatorIdentifier:     &fromAddress,
+			InitiatorIdentifier:     getInitator(&req),
 			ThreadId:                &threadId,
-			ParticipantsIdentifiers: toStringArr(append(email.To, email.Cc...)),
+			ParticipantsIdentifiers: participants,
 		}
+		//Store the message in message store
+		msConn := util.GetMessageStoreConnection(c, df)
+		defer util.CloseMessageStoreConnection(msConn)
+		msClient := ms.NewMessageStoreServiceClient(msConn)
+
+		ctx := context.Background()
+		ctx = metadata.AppendToOutgoingContext(ctx, service.ApiKeyHeader, conf.Service.MessageStoreApiKey)
+		ctx = metadata.AppendToOutgoingContext(ctx, service.UsernameHeader, getUser(&req))
+
+		savedMessage, err := msClient.SaveMessage(ctx, message)
+		if err != nil {
+			se, _ := status.FromError(err)
+			log.Printf("failed creating message item: status=%s message=%s", se.Code(), se.Message())
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"result": fmt.Sprintf("failed creating message item: status=%s message=%s", se.Code(), se.Message()),
+			})
+			return
+		}
+		log.Printf("message item created with id: %s", savedMessage.GetConversationEventId())
+
+		//Set up a connection to the oasis-api server.
+		oasisConn := GetOasisClient(c, df)
+		defer closeOasisConnection(oasisConn)
+		oasisClient := o.NewOasisApiServiceClient(oasisConn)
+		_, mEventErr := oasisClient.NewMessageEvent(ctx, &o.NewMessage{ConversationId: savedMessage.ConversationId, ConversationItemId: savedMessage.GetConversationEventId()})
+		if mEventErr != nil {
+			se, _ := status.FromError(mEventErr)
+			log.Printf("failed new message event: status=%s message=%s", se.Code(), se.Message())
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": fmt.Sprintf("message item created with id: %s", savedMessage.GetConversationEventId()),
+		})
+	})
 }
